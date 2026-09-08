1ffd924f837ea880a7ccaf828483dc10b894e514
aliramezan5 <aliramezan5@atomicmail.io>
2026-09-08 19:27:20 +0330

    Refresh PWA cache for fintech redesign

--- check-calculator/sw.js
+++ check-calculator/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'check-calculator-v2';
+const CACHE = 'check-calculator-v3-fintech';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   self.skipWaiting();
